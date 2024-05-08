2e756c2e28f1518970b012134b2940ee5f9cb8e4
Thomas Murphy <thomasmurphy1991@gmail.com>
2024-05-07 20:54:39 -0400

    TDD Book Ch1 + 2

--- CH1/Company.Math/obj/Debug/net8.0/Company.Math.AssemblyInfo.cs
+++ CH1/Company.Math/obj/Debug/net8.0/Company.Math.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Company.Math")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34d8618c361ac82aa018deb09d349b127e73f80a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Company.Math")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Company.Math")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
